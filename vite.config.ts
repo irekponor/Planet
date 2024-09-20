68445acf0b614d3d9116dc31a8ea5c1d96d2d015
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-21 00:45:57 +0100

    deploy issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
