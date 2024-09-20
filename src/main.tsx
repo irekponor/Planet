b08bd7bad4681a464bf966bb7fd2e4e593c74a02
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-20 03:46:16 +0100

    static typing

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>{<App />}</BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
