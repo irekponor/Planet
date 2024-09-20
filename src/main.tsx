f91864cb511e855ff7ef0a5985bc4a4fe3de5ae2
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-20 02:48:23 +0100

    static typing

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <Route path="*" element={<App />} />
   </BrowserRouter>
 );
