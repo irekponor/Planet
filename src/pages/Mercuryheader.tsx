3f70918725b8677710c610d4aeea26387ce4add1
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-20 04:38:52 +0100

    static typing

--- src/pages/Mercuryheader.tsx
+++ src/pages/Mercuryheader.tsx
@@ -7,7 +7,7 @@ const Mercuryheader = () => {
           <div>
             <div className="mr-10">
               <a
-                href="/index"
+                href="/main"
                 className="no-underline p-3 justify-between font-semibold font-title text-white text-[18px]"
               >
                 Home
