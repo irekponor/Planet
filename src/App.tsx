9ebf9afa2b7219696f60c201c326608ca35d3c7d
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-21 04:06:15 +0100

    host

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import Responsive from "./components/Responsive";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Solarsystem from "./pages/Solarsystem";
+import DYK from "./pages/DYK";
+import Contact from "./pages/Contact";
+import Mercury from "./pages/Mercury";
+
 function App() {
   return (
     <div>
-      <Responsive />
+      <BrowserRouter basename="/Planet/">
+        <Responsive />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="main" element={<Home />} />
+          <Route path="space" element={<Solarsystem />} />
+          <Route path="news" element={<DYK />} />
+          <Route path="message" element={<Contact />} />
+          <Route path="planet" element={<Mercury />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,7 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import Solarsystem from "./pages/Solarsystem";
-import DYK from "./pages/DYK";
-import Contact from "./pages/Contact";
-import Mercury from "./pages/Mercury";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/Planet/">
-    <App />
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="main" element={<Home />} />
-      <Route path="space" element={<Solarsystem />} />
-      <Route path="news" element={<DYK />} />
-      <Route path="message" element={<Contact />} />
-      <Route path="planet" element={<Mercury />} />
-    </Routes>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
