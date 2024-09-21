201306ba09bf59277bc7214be78dee6952fb6933
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-21 03:08:18 +0100

    deploy issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import Responsive from "./components/Responsive";
-import Home from "./pages/Home";
-import Solarsystem from "./pages/Solarsystem";
-import DYK from "./pages/DYK";
-import Contact from "./pages/Contact";
-import Mercury from "./pages/Mercury";
 
 function App() {
   return (
     <div>
       <Responsive />
-      <Home />
-      <Solarsystem />
-      <DYK />
-      <Contact />
-      <Mercury />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Solarsystem from "./pages/Solarsystem";
+import DYK from "./pages/DYK";
+import Contact from "./pages/Contact";
+import Mercury from "./pages/Mercury";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/planet/">
     <Routes>
       <Route index element={<App />} />
+      <Route path="main" element={<Home />} />
+      <Route path="space" element={<Solarsystem />} />
+      <Route path="news" element={<DYK />} />
+      <Route path="message" element={<Contact />} />
+      <Route path="planet" element={<Mercury />} />
     </Routes>
   </BrowserRouter>
 );
