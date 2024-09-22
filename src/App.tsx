f3da75de0a57c8ef4f56cce6e612e02e22edc4aa
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-22 12:14:29 +0100

    html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Responsive from "./components/Responsive";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Solarsystem from "./pages/Solarsystem";
 import DYK from "./pages/DYK";
@@ -8,20 +10,32 @@ import Mercury from "./pages/Mercury";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check for redirect query parameters
+    const params = new URLSearchParams(location.search);
+    const redirectPath = params.get("redirect");
+
+    if (redirectPath) {
+      // Remove the redirect query parameter and navigate to the original path
+      navigate(redirectPath, { replace: true });
+    }
+  }, [location, navigate]);
+
   return (
     <div>
-      <BrowserRouter basename="/Planet/">
-        <Responsive />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="space" element={<Solarsystem />} />
-          <Route path="news" element={<DYK />} />
-          <Route path="message" element={<Contact />} />
-          <Route path="planet" element={<Mercury />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Responsive />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="space" element={<Solarsystem />} />
+        <Route path="news" element={<DYK />} />
+        <Route path="message" element={<Contact />} />
+        <Route path="planet" element={<Mercury />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,10 @@ import App from "./App.tsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/Planet/">
+    <App />
+  </BrowserRouter>
+);
